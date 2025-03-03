cdbf2c0a4bee8c69a06a2f65a33a3fc1e5e307ac
Thv 1364 <jmonson6@outlook.com>
2025-03-03 19:38:06 +0000

    f

---
+++ e.py
@@ -0,0 +1,24 @@
+#!!!!!!!DELETE LATER!!!!!!!!!
+#!!!!!!!DELETE LATER!!!!!!!!!
+#!!!!!!!DELETE LATER!!!!!!!!!
+#!!!!!!!DELETE LATER!!!!!!!!!
+#!!!!!!!DELETE LATER!!!!!!!!!
+
+#Complete? Need:
+
+#Nope      Input with instructions
+#Nope      List
+#Nope      Function
+#Nope      Parameter to function
+#Nope      Loops
+#Nope      If
+#Nope      Variable changing with lists
+#Nope      Call function
+
+total_amount = int(input("How many total assigments (including tests) would you like to \"grade\"?"))
+current_graded=0
+
+while current_graded<total_amount:
+    current_graded=current_graded+1
+
+
