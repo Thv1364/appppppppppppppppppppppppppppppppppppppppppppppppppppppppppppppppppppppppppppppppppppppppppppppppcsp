df096de4451407646bfecb698a4c9627f2cd5ec7
Thv 1364 <jmonson6@outlook.com>
2025-03-05 18:32:52 +0000

    f

--- e.py
+++ e.py
@@ -15,10 +15,38 @@
 #Nope      Variable changing with lists
 #Nope      Call function
 
-total_amount = int(input("How many total assigments (including tests) would you like to \"grade\"?"))
+assignments={
+    "a":[0,10]
+    }
+
+total_amount = int(input("How many total assigments (including tests) would you like to \"grade\"? \n"))
 current_graded=0
 
+
+
+def add_assignment(assignment, done, outof):
+    if done=="yes":
+        done=1
+    else:
+        done=0
+    assignments[assignment]=[done,outof]
+
+
+
+# def averageit():
+#     nume=0
+#     denom=0
+#     for assignment in assignments:
+#         print("it is" + assignment)
+#         nume=intassignment[0]+int(nume)
+#         denom=assignment[1]+denom
+#     avg=nume/denom
+#     print(str(avg))
+
+averageit()
+
 while current_graded<total_amount:
+    add_assignment(input("What is the assignment named?"),input("Was the assignment submitted? Type yes or no."),int(input("Out of how many points?")))
     current_graded=current_graded+1
 
 
